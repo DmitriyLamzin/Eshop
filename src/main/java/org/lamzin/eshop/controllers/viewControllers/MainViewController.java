f155936a8cceab5909f37000828a7ac0e300e33e
Dmitriy Lamzin <dmitriy.lamzin@gmail.com>
2016-07-12 11:15:03 +0600

    adding a controller pakage to the project

---
+++ src/main/java/org/lamzin/eshop/controllers/viewControllers/MainViewController.java
@@ -0,0 +1,20 @@
+package org.lamzin.eshop.controllers.viewControllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by Dmitriy on 27.06.2016.
+ */
+@Controller
+@RequestMapping("/main")
+public class MainViewController {
+    @RequestMapping(
+            method = RequestMethod.GET)
+    public ModelAndView getAdminPage(){
+        ModelAndView modelandview = new ModelAndView("main");
+        return modelandview;
+    }
+}
