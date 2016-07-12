9d403d47ef30b73799cea2403b40e651536c5d40
Dmitriy Lamzin <dmitriy.lamzin@gmail.com>
2016-07-12 11:10:29 +0600

    initial pom file befor first commit, and initial ControllerLoggingAspekt before first commit

---
+++ src/main/java/org/lamzin/eshop/aspects/ControllerLoggingAspect.java
@@ -0,0 +1,64 @@
+package org.lamzin.eshop.aspects;
+
+import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+/**
+ * Created by Dmitriy on 16/02/2016.
+ */
+@Aspect
+public class ControllerLoggingAspect {
+    private final Logger log = Logger.getLogger(this.getClass());
+
+    @Before("execution(* org.lamzin.eshop.controllers.rest.*.*(..))")
+    public void beforeMethodLog (JoinPoint joinPoint) throws Throwable {
+
+        StringBuffer logMessage = getMethodDescription(joinPoint);
+
+        logMessage.append(" is called...");
+
+        log.info(logMessage.toString());
+    }
+
+
+    @AfterReturning(value = "execution(* org.lamzin.eshop.controllers.rest.*.*(..))",
+            returning = "retVal")
+    public void afterInvocationLogger(JoinPoint joinPoint, Object retVal){
+        StringBuffer logMessage = getMethodDescription(joinPoint);
+        logMessage.append(" is complete, ret. val.: ");
+        if (retVal != null)
+            logMessage.append(retVal.toString());
+        else logMessage.append("null");
+        log.debug(logMessage.toString());
+    }
+
+    private StringBuffer getMethodDescription(JoinPoint joinPoint) {
+        StringBuffer logMessage = new StringBuffer();
+        logMessage.append(joinPoint.getTarget().getClass().getName());
+        logMessage.append(".");
+        logMessage.append(joinPoint.getSignature().getName());
+        logMessage.append("(");
+        // append args
+        Object[] args = joinPoint.getArgs();
+        for (Object arg : args) {
+            logMessage.append(arg).append(",");
+        }
+        if (args.length > 0) {
+            logMessage.deleteCharAt(logMessage.length() - 1);
+        }
+        logMessage.append(")");
+        return logMessage;
+    }
+
+    @AfterThrowing(value = "execution(* org.lamzin.eshop.controllers.rest.*.*(..))", throwing = "throwable")
+    public void afterThrowingLogger(JoinPoint joinPoint, Throwable throwable){
+        StringBuffer logMessage = getMethodDescription(joinPoint);
+        logMessage.append(" throws: ").
+                append(throwable.getMessage());
+        log.error(logMessage.toString());
+    }
+}
