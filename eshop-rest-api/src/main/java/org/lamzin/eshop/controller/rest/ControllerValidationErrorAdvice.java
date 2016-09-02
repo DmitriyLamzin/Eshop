0e83d272d5c3011879934303940c8c2f6441062e
Dmitriy Lamzin <dmitriy.lamzin@gmail.com>
2016-09-02 10:48:32 +0600

    Add a localization to the rest error messages. Support RU and EN locale

--- eshop-rest-api/src/main/java/org/lamzin/eshop/controller/rest/ControllerValidationErrorAdvice.java
+++ eshop-rest-api/src/main/java/org/lamzin/eshop/controller/rest/ControllerValidationErrorAdvice.java
@@ -1,6 +1,9 @@
 package org.lamzin.eshop.controller.rest;
 
 import org.lamzin.eshop.dto.ValidationErrorDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -11,12 +14,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by Dmitriy on 31.03.2016.
  */
 @ControllerAdvice
 public class ControllerValidationErrorAdvice {
+    @Autowired
+    private MessageSource messageSource;
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
@@ -31,10 +37,17 @@ public class ControllerValidationErrorAdvice {
         ValidationErrorDTO dto = new ValidationErrorDTO();
 
         for (FieldError fieldError: fieldErrors) {
-            dto.addFieldError(fieldError.getField(), fieldError.getDefaultMessage());
+            String localizedMessage = localizeMessage(fieldError.getCode());
+            dto.addFieldError(fieldError.getField(), localizedMessage);
         }
 
         return dto;
+    }
+
+    private String localizeMessage(String code) {
+        Locale locale = LocaleContextHolder.getLocale();
+        String errorMessage = messageSource.getMessage(code, null, locale);
+        return errorMessage;
     }
 
 }
