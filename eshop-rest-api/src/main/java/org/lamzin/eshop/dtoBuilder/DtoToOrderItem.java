d9338348e8c319ef0dc9d2af395bf8d8fe8a70b4
Dmitriy Lamzin <dmitriy.lamzin@gmail.com>
2016-10-15 16:11:50 +0600

    DtoToOrderItemBuilder

---
+++ eshop-rest-api/src/main/java/org/lamzin/eshop/dtoBuilder/DtoToOrderItem.java
@@ -0,0 +1,30 @@
+package org.lamzin.eshop.dtoBuilder;
+
+import org.lamzin.eshop.dto.OrderItemDto;
+import org.lamzin.eshop.model.OrderItem;
+import org.lamzin.eshop.service.interfaces.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Dmitriy on 26.09.2016.
+ */
+@Service
+public class DtoToOrderItem {
+
+    @Autowired
+    ProductService productService;
+    public List<OrderItem> buildOrderItems(List<OrderItemDto> orderItems) {
+        List<OrderItem> orderItemList = new ArrayList<OrderItem>();
+        for (OrderItemDto orderItemDto : orderItems){
+            OrderItem orderItem = new OrderItem();
+            orderItem.setSize(orderItemDto.getSize());
+            orderItem.setProduct(productService.getProductById(orderItemDto.getProduct().getProductId()));
+            orderItemList.add(orderItem);
+        }
+        return orderItemList;
+    }
+}
---
+++ eshop-rest-api/src/main/java/org/lamzin/eshop/dtoBuilder/OrderItemDtoBuilter.java
@@ -0,0 +1,36 @@
+package org.lamzin.eshop.dtoBuilder;
+
+import org.lamzin.eshop.controller.rest.CardController;
+import org.lamzin.eshop.dto.OrderCardExtendedDto;
+import org.lamzin.eshop.dto.OrderItemDto;
+import org.lamzin.eshop.model.OrderItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by Dmitriy on 26.09.2016.
+ */
+@Service
+public class OrderItemDtoBuilter {
+
+    @Autowired
+    ProductDtoBuilder productDtoBuilder;
+
+    public List<OrderItemDto> createListBasicDto(List<OrderItem> orderItems, OrderCardExtendedDto orderCardExtendedDto) {
+        List<OrderItemDto> orderItemDtos = new LinkedList<OrderItemDto>();
+
+        for (OrderItem orderItem : orderItems){
+            OrderItemDto orderItemDto = new OrderItemDto(productDtoBuilder.createBasicDto(orderItem.getProduct()),
+                    orderItem.getSize(),
+                    orderItem.getPrice());
+            orderItemDto.add(ControllerLinkBuilder.linkTo(ControllerLinkBuilder.methodOn(CardController.class).
+                    deleteOrderItem(orderItem.getProduct().getId(), orderCardExtendedDto)).withSelfRel());
+            orderItemDtos.add(orderItemDto);
+        }
+        return orderItemDtos;
+    }
+}
