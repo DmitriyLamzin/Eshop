6f2d85bb4eb490adf25df4784391907307c4c06d
Dmitriy Lamzin <dmitriy.lamzin@gmail.com>
2016-10-24 13:09:13 +0600

    The bug with criteriaQuery was removed

--- eshop-persist/src/main/java/org/lamzin/eshop/dao/impl/ProductDaoJpaImpl.java
+++ eshop-persist/src/main/java/org/lamzin/eshop/dao/impl/ProductDaoJpaImpl.java
@@ -1,5 +1,6 @@
 package org.lamzin.eshop.dao.impl;
 
+import org.apache.log4j.Logger;
 import org.lamzin.eshop.model.catalog.Category;
 import org.lamzin.eshop.model.catalog.Product;
 import org.lamzin.eshop.model.catalog.SubCategory;
@@ -20,11 +21,14 @@ import java.util.List;
 @Repository ("productDao")
 public class ProductDaoJpaImpl extends GenericDaoJpaImpl<Product, Long> implements ProductDao {
 
+    private final Logger logger = Logger.getLogger(this.getClass());
     public ProductDaoJpaImpl() {
         super(Product.class);
     }
 
     public Product findById(String categoryId, String subcategoryId, long productId) {
+        logger.info("method findById with arguments " + categoryId + " " + subcategoryId + " " + productId +
+        " is called");
         CriteriaBuilder builder = em.getCriteriaBuilder();
         CriteriaQuery<Product> criteriaQuery = builder.createQuery(Product.class);
         Root<Product> p = criteriaQuery.from(Product.class);
@@ -52,25 +56,34 @@ public class ProductDaoJpaImpl extends GenericDaoJpaImpl<Product, Long> implemen
                                              int page,
                                              int amountPerPage) {
 
+        logger.info("method getMultipleProducts with arguments " + subCategory +
+                " " + minPrice + " " + maxPrice + " " + producer + " " + orderBy +
+                " " + page + " " + amountPerPage + " is called");
         CriteriaBuilder builder = em.getCriteriaBuilder();
         CriteriaQuery<Product> criteriaQuery = builder.createQuery(Product.class);
         Root<Product> p = criteriaQuery.from(Product.class);
         Join<Product, SubCategory> sc = p.join("subCategory");
         criteriaQuery.select(p);
 
-            criteriaQuery.where(
-                    builder.equal(sc.get("subCategoryId").as(String.class), subCategory),
-                    builder.ge(p.get("price").as(Double.class), minPrice),
-                    builder.le(p.get("price").as(Double.class), maxPrice));
+        List<Predicate> predicates = new ArrayList<Predicate>();
+
+        predicates.add(builder.equal(sc.get("subCategoryId").as(String.class), subCategory));
+        predicates.add(builder.ge(p.get("price").as(Double.class), minPrice));
+        predicates.add(builder.le(p.get("price").as(Double.class), maxPrice));
 
         if (!producer.isEmpty() && !producer.contains("all")){
-            List<Predicate> predicates = new ArrayList<Predicate>();
+            List<Predicate> producerPredicates = new ArrayList<Predicate>();
             Iterator<String> iterator = producer.iterator();
-            predicates.add(builder.equal(p.get("producer").as(String.class), iterator.next()));
+            producerPredicates.add(builder.equal(p.get("producer").as(String.class), iterator.next()));
             while (iterator.hasNext()){
-                predicates.add(builder.or(builder.equal(p.get("producer").as(String.class), iterator.next())));
+                producerPredicates.add(builder.or(builder.equal(p.get("producer").as(String.class), iterator.next())));
             }
-            criteriaQuery.where(builder.or(predicates.toArray(new Predicate[predicates.size()])));
+            criteriaQuery.where(
+                    builder.or(producerPredicates.toArray(new Predicate[producerPredicates.size()])),
+                    builder.and(predicates.toArray(new Predicate[predicates.size()])));
+        }else {
+            criteriaQuery.where(
+                    builder.and(predicates.toArray(new Predicate[predicates.size()])));
         }
 
         criteriaQuery.orderBy(builder.asc(p.get(orderBy)));
@@ -116,19 +129,25 @@ public class ProductDaoJpaImpl extends GenericDaoJpaImpl<Product, Long> implemen
 
         criteriaQuery.select(builder.count(p));
 
-        criteriaQuery.where(
-                builder.equal(sc.get("subCategoryId").as(String.class), subCategory),
-                builder.ge(p.get("price").as(Double.class), minPrice),
-                builder.le(p.get("price").as(Double.class), maxPrice));
+        List<Predicate> predicates = new ArrayList<Predicate>();
+
+        predicates.add(builder.equal(sc.get("subCategoryId").as(String.class), subCategory));
+        predicates.add(builder.ge(p.get("price").as(Double.class), minPrice));
+        predicates.add(builder.le(p.get("price").as(Double.class), maxPrice));
 
         if (!producer.isEmpty() && !producer.contains("all")){
-            List<Predicate> predicates = new ArrayList<Predicate>();
+            List<Predicate> producerPredicates = new ArrayList<Predicate>();
             Iterator<String> iterator = producer.iterator();
-            predicates.add(builder.equal(p.get("producer").as(String.class), iterator.next()));
+            producerPredicates.add(builder.equal(p.get("producer").as(String.class), iterator.next()));
             while (iterator.hasNext()){
-                predicates.add(builder.or(builder.equal(p.get("producer").as(String.class), iterator.next())));
+                producerPredicates.add(builder.or(builder.equal(p.get("producer").as(String.class), iterator.next())));
             }
-            criteriaQuery.where(builder.or(predicates.toArray(new Predicate[predicates.size()])));
+            criteriaQuery.where(
+                    builder.or(producerPredicates.toArray(new Predicate[producerPredicates.size()])),
+                    builder.and(predicates.toArray(new Predicate[predicates.size()])));
+        }else {
+            criteriaQuery.where(
+                    builder.and(predicates.toArray(new Predicate[predicates.size()])));
         }
 
        return em.createQuery(criteriaQuery).getSingleResult();
